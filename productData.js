20fef418a68fb12e42a1e0eff904c7b79499bd17
thinhvpg2410 <thinh.vpg@gmail.com>
2024-09-19 11:21:33 +0700

    [ADD] Create example  JSON data and product's image

---
+++ productData.js
@@ -0,0 +1,27 @@
+export const productData = {
+    "id": "1",
+    "name": "Điện thoại Vinsmart Joy 3 - Hàng chính hãng",
+    "price": 1790000,
+    "color": [
+        {
+            "id": "#e3d8de",
+            "name": "Bạc",
+            "image-url": "./assets/product/vs_silver.png"
+        },
+        {
+            "id": "#F30D0D",
+            "name": "Đỏ",
+            "image-url": "./assets/product/vs_red.png"
+        },
+        {
+            "id": "#000000",
+            "name": "Đen",
+            "image-url": "./assets/product/vs_black.png"
+        },
+        {
+            "id": "#234896",
+            "name": "Xanh Dương",
+            "image-url": "./assets/product/vs_blue.png"
+        }
+    ]
+}
