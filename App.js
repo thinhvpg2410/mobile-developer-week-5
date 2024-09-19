7825a8c94f57983085ab62422e67dcc4a092a66d
thinhvpg2410 <thinh.vpg@gmail.com>
2024-09-19 10:11:24 +0700

    [ADD] Develop static screen 1 ProductPage

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {View, Text, Image, TouchableOpacity, StyleSheet} from 'react-native';
+import ProductPage from "./Screen/ProductPage";
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
 
+const stack = createNativeStackNavigator();
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <ProductPage/>
+        </NavigationContainer>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Screen/ProductPage.jsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import {TouchableOpacity, View, Text, Image, StyleSheet} from "react-native";
+
+const ProductPage = ({navigation}) => {
+    return (
+        <View style={styles.container}>
+            <Image
+                source={{uri: './assets/product/vs_silver.png'}} // Replace with actual image URL
+                style={styles.productImage}
+            />
+
+            <Text style={styles.productTitle}>
+                Điện Thoại Vsmart Joy 3 - Hàng chính hãng
+            </Text>
+
+            <View style={styles.ratingContainer}>
+                <Text style={{
+                    fontSize: 24,
+                    color: '#FFD700'
+                }}>★★★★★</Text>
+                <Text style={{
+                    marginLeft: 5,
+                    color: '#888888',
+                }}>(Xem 828 đánh giá)</Text>
+            </View>
+
+            <View style={styles.priceContainer}>
+                <Text style={styles.currentPrice}>1.790.000 đ</Text>
+                <Text style={styles.originalPrice}>1.790.000 đ</Text>
+            </View>
+
+            <TouchableOpacity style={styles.colorSelectionButton}>
+                <Text style={{
+                    textAlign: 'center',
+                    color: '#333',
+                }}>4 MÀU - CHỌN MÀU</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.buyButton}>
+                <Text style={styles.buyButtonText}>CHỌN MUA</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 10,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+    },
+    productImage: {
+        width: '100%',
+        height: 400,
+        resizeMode: 'contain',
+        borderRadius: 10,
+    },
+    productTitle: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginTop: 20,
+    },
+    ratingContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 5,
+
+    },
+    priceContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 10,
+    },
+    currentPrice: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#ff0000',
+    },
+    originalPrice: {
+        fontSize: 14,
+        textDecorationLine: 'line-through',
+        color: '#888888',
+        marginLeft: 10,
+    },
+    colorSelectionButton: {
+        backgroundColor: '#f2f2f2',
+        padding: 10,
+        borderRadius: 5,
+        marginTop: 10,
+    },
+    buyButton: {
+        backgroundColor: '#ff0000',
+        padding: 15,
+        borderRadius: 5,
+        marginTop: 15,
+        verticalAlign:'bottom',
+        justifyContent: 'flex-end',
+
+    },
+    buyButtonText: {
+        textAlign: 'center',
+        color: '#fff',
+        fontWeight: 'bold',
+    },
+});
+
+export default ProductPage;
